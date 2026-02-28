1e707939e2d85b65cff395d7ed5ede83cc434f4b
Tamer Bsharat <tamr.bsharat@gmail.com>
2026-02-28 05:12:04 +0200

    first commit

---
+++ KASHOP.BLL/Service/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using KASHOP.DAL.DTO.Request;
+using KASHOP.DAL.DTO.Response;
+
+namespace KASHOP.BLL.Service
+{
+    public interface ICategoryService
+    {
+        Task <List<CategoryResponse>> GetAllCategories();
+        Task<CategoryResponse> CreateCategory(CategoryRequest request);
+    }
+}
---
+++ KASHOP.DAL/Repository/MockCategoryRep.cs
@@ -0,0 +1,32 @@
+﻿using KASHOP.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Repository
+{
+    internal class MockCategoryRep : ICategoryRepository
+    {
+        public Category Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> CreateAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
